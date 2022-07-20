58e30fff57ece41ac1669f7f30a88bff81079106
AlbertMT10 <frankcoaquirajusto4@gmail.com>
2022-07-20 18:14:36 -0500

    fl

--- petitocelulares/src/main/java/pe/edu/upeu/celulares/App.java
+++ petitocelulares/src/main/java/pe/edu/upeu/celulares/App.java
@@ -4,10 +4,9 @@ package pe.edu.upeu.celulares;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        new MenuOpciones().menuPrincipal();
+        
     }
 }
